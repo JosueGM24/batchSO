54044be4629c9c5352c696d67d1ae9de400bb1cd
Daniel <jos.god.mel.10@gmail.com>
2023-07-20 13:22:22 -0600

    all changues saved ver 1.0.1

--- resistencias.bat
+++ resistencias.bat
@@ -17,6 +17,23 @@ get_resistor_value() {
     esac
 }
 
+# Función para obtener el valor multiplicador en ohmios
+get_resistor_multiplier() {
+    case $1 in
+        ne|negro) echo 1 ;;
+        ma|marron) echo 10 ;;
+        ro|rojo) echo 100 ;;
+        na|naranja) echo 1000 ;;
+        am|amarillo) echo 10000 ;;
+        ve|verde) echo 100000 ;;
+        az|azul) echo 1000000 ;;
+        vi|violeta) echo 10000000 ;;
+        gr|gris) echo 100000000 ;;
+        bl|blanco) echo 1000000000 ;;
+        *) echo -1 ;; # Valor inválido
+    esac
+}
+
 # Obtener los colores ingresados
 echo "Ingresa los colores de la resistencia (separados por comas): "
 read colores
@@ -27,9 +44,10 @@ colores=$(echo "$colores" | tr '[:upper:]' '[:lower:]' | sed 's/ //g')
 # Separar los colores por comas
 IFS=',' read -ra colores_array <<< "$colores"
 
-# Obtener el valor numérico de cada color y calcular el valor en ohmios
+# Obtener el valor numérico de los dos primeros colores
 valor_ohmios=""
-for color in "${colores_array[@]}"; do
+for (( i = 0; i < 2; i++ )); do
+    color=${colores_array[$i]}
     valor=$(get_resistor_value "$color")
     if [ $valor -eq -1 ]; then
         echo "Color inválido: $color"
@@ -38,7 +56,15 @@ for color in "${colores_array[@]}"; do
     valor_ohmios="${valor_ohmios}${valor}"
 done
 
+# Obtener el valor multiplicador del tercer color
+color=${colores_array[2]}
+valor_multiplicador=$(get_resistor_multiplier "$color")
+if [ $valor_multiplicador -eq -1 ]; then
+    echo "Color multiplicador inválido: $color"
+    exit 1
+fi
+
 # Calcular el valor en ohmios
-valor_final=$(( valor_ohmios * 10**${#valor_ohmios} ))
+valor_final=$(( valor_ohmios * valor_multiplicador ))
 
 echo "El valor de la resistencia es: $valor_final ohms"
