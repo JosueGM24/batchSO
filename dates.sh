447cfc5775701afe46c948b7cfcd15589db4fcac
Daniel <jos.god.mel.10@gmail.com>
2023-07-27 14:10:06 -0600

    1.5

--- dates.sh
+++ dates.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-# Función para convertir una fecha a segundos desde el epoch (1970-01-01 00:00:00 UTC)
-function fecha_a_segundos() {
-    date -d "$1" +%s
-}
-
-# Función para calcular la diferencia entre dos fechas en segundos
+# Función para calcular la diferencia entre dos fechas en días, horas y meses
 function calcular_diferencia() {
     fecha_inicial=$1
     fecha_final=$2
 
-    segundos_inicial=$(fecha_a_segundos "$fecha_inicial")
-    segundos_final=$(fecha_a_segundos "$fecha_final")
+    # Convertir fechas a timestamp (segundos desde el epoch)
+    timestamp_inicial=$(date -d "$fecha_inicial" +%s)
+    timestamp_final=$(date -d "$fecha_final" +%s)
 
-    echo $((segundos_final - segundos_inicial))
-}
+    # Calcular la diferencia en segundos
+    diferencia_segundos=$((timestamp_final - timestamp_inicial))
 
-# Función para convertir segundos a días, horas y meses
-function segundos_a_dias_horas_meses() {
-    segundos=$1
+    # Calcular meses y días
+    meses=$((diferencia_segundos / (30 * 24 * 60 * 60))
+    dias=$((diferencia_segundos / (24 * 60 * 60))
 
-    meses=$((segundos / (30*24*60*60)))
-    segundos=$((segundos % (30*24*60*60)))
-
-    dias=$((segundos / (24*60*60)))
-    segundos=$((segundos % (24*60*60)))
-
-    horas=$((segundos / (60*60)))
-    segundos=$((segundos % (60*60)))
+    # Calcular horas
+    segundos_restantes=$((diferencia_segundos % (24 * 60 * 60))
+    horas=$((segundos_restantes / (60 * 60))
 
     echo "Días: $dias"
     echo "Horas: $horas"
@@ -38,8 +29,5 @@ function segundos_a_dias_horas_meses() {
 read -p "Ingrese la fecha inicial (formato dd/mm/yyyy-hh:mm:ss): " fecha_inicial
 read -p "Ingrese la fecha final (formato dd/mm/yyyy-hh:mm:ss): " fecha_final
 
-# Calcular diferencia en segundos
-diferencia_segundos=$(calcular_diferencia "$fecha_inicial" "$fecha_final")
-
-# Convertir segundos a días, horas y meses y mostrar el resultado
-segundos_a_dias_horas_meses $diferencia_segundos
+# Calcular diferencia y mostrar el resultado
+calcular_diferencia "$fecha_inicial" "$fecha_final"
