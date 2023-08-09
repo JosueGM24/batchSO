b44f943473b354bcd0a077189032e694b93c99c8
Daniel <58919248+JosueGM24@users.noreply.github.com>
2023-08-09 09:40:20 -0600

    Update mult.sh

--- mult.sh
+++ mult.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
-# Función para multiplicar dos monomios
-multiplicar_monomios() {
-    local expr1="$1"
-    local expr2="$2"
-    resultado=$(echo "expand(($expr1) * ($expr2))" | bc -l)
-    echo "$resultado"
-}
-
 # Obtener los monomios del usuario
 echo "Ingresa los monomios separados por espacios (ejemplo: (x+1) (x+2) ...):"
 read expresiones
 
-# Reemplazar 'x' con 'x' para que bc pueda entenderlo
-expresiones="${expresiones//x/x}"
+# Crear un archivo de entrada para Maxima
+echo "load(simplify_sum);" > input.mac
+echo "expand("$(echo "$expresiones" | sed 's/+/\\+/g')");" >> input.mac
+echo "quit();" >> input.mac
 
-# Separar las expresiones en monomios individuales
-monomios=($(echo "$expresiones"))
+# Ejecutar Maxima y guardar el resultado en un archivo
+maxima -b input.mac > output.txt
 
-# Calcular la multiplicación de los monomios
-resultado="${monomios[0]}"
-for ((i = 1; i < ${#monomios[@]}; i++)); do
-    resultado=$(multiplicar_monomios "$resultado" "${monomios[i]}")
-done
-
-# Reemplazar 'x' con 'x' para mostrar el resultado en formato legible
-resultado="${resultado//x/x}"
+# Extraer el resultado del archivo
+resultado=$(sed -n '/(%o1)/s///p' output.txt)
 
 # Mostrar el resultado
 echo "El resultado es:"
 echo "$resultado"
+
+# Eliminar archivos temporales
+rm input.mac output.txt
