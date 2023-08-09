cb198207edac77a1445c24913c3c57f57f24b371
Daniel <58919248+JosueGM24@users.noreply.github.com>
2023-08-09 09:37:11 -0600

    Update mult.sh

--- mult.sh
+++ mult.sh
@@ -4,13 +4,16 @@
 multiplicar_monomios() {
     local expr1="$1"
     local expr2="$2"
-    resultado=$(echo "$expr1 * $expr2" | bc -l)
+    resultado=$(echo "expand(($expr1) * ($expr2))" | bc -l)
     echo "$resultado"
 }
 
 # Obtener los monomios del usuario
 echo "Ingresa los monomios separados por espacios (ejemplo: (x+1) (x+2) ...):"
 read expresiones
+
+# Reemplazar 'x' con 'x' para que bc pueda entenderlo
+expresiones="${expresiones//x/x}"
 
 # Separar las expresiones en monomios individuales
 monomios=($(echo "$expresiones"))
@@ -20,6 +23,9 @@ resultado="${monomios[0]}"
 for ((i = 1; i < ${#monomios[@]}; i++)); do
     resultado=$(multiplicar_monomios "$resultado" "${monomios[i]}")
 done
+
+# Reemplazar 'x' con 'x' para mostrar el resultado en formato legible
+resultado="${resultado//x/x}"
 
 # Mostrar el resultado
 echo "El resultado es:"
