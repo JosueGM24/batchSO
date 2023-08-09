5e140acf7d783299f49dccf183e76094d6153a3f
Daniel <58919248+JosueGM24@users.noreply.github.com>
2023-08-09 09:30:48 -0600

    Update mult.sh

--- mult.sh
+++ mult.sh
@@ -1,39 +1,26 @@
 #!/bin/bash
 
-# Función para multiplicar dos polinomios en formato (x+a)
-multiply_polynomials() {
-    local result=""
-
-    # Iterar a través de los factores y construir el polinomio resultante
-    for factor in "$@"; do
-        if [[ $factor =~ ^\([[:space:]]*x[[:space:]]*\+[[:space:]]*([0-9-]+)[[:space:]]*\)$ ]]; then
-            coefficient="${BASH_REMATCH[1]}"
-            if [ "$coefficient" -gt 0 ]; then
-                term="+${coefficient}x"
-            elif [ "$coefficient" -lt 0 ]; then
-                term="${coefficient}x"
-            fi
-        fi
-        result="${result}${term}"
-    done
-
-    # Agregar el término constante si es necesario
-    if [[ "$result" != "" ]]; then
-        result="${result}+"
-    fi
-    result="${result}${BASH_REMATCH[1]}"
-
-    # Eliminar el primer signo de suma si existe y ajustar el formato
-    result="${result#?}"
-    result="${result//x^1/x}"
-    echo "$result"
+# Función para multiplicar dos monomios
+multiplicar_monomios() {
+    local expr1="$1"
+    local expr2="$2"
+    resultado=$(echo "$expr1 * $expr2" | bc -l)
+    echo "$resultado"
 }
 
-# Lectura de las expresiones de usuario
-read -p "Introducir expresiones (separadas por espacios): " expressions
-expression_array=($expressions)
+# Obtener los monomios del usuario
+echo "Ingresa los monomios separados por espacios (ejemplo: (x+1) (x+2) ...):"
+read expresiones
 
-# Llamada a la función para multiplicar los polinomios
-result=$(multiply_polynomials "${expression_array[@]}")
+# Separar las expresiones en monomios individuales
+monomios=($(echo "$expresiones"))
 
-echo "Resultado de la multiplicación de polinomios = $result"
+# Calcular la multiplicación de los monomios
+resultado="${monomios[0]}"
+for ((i = 1; i < ${#monomios[@]}; i++)); do
+    resultado=$(multiplicar_monomios "$resultado" "${monomios[i]}")
+done
+
+# Mostrar el resultado
+echo "El resultado es:"
+echo "$resultado"
